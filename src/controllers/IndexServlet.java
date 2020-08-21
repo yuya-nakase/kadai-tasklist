8e7f08e80d1e5bc5a0c531bea78c6e90ed2682d3
yuya.nakase <red.arc1205@gmail.com>
2020-08-21 20:13:26 +0900

    Add pagination

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -35,12 +35,24 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
+        int page = 1;
+        try{
+            page = Integer.parseInt(request.getParameter("page"));
+        }catch(NumberFormatException e){}
+
         List<Task> tasks = em.createNamedQuery("getAllTasks",Task.class)
+                             .setFirstResult(15 * (page - 1))
+                             .setMaxResults(15)
                              .getResultList();
+
+        long tasks_count = (long)em.createNamedQuery("getTaskCount",Long.class)
+                                   .getSingleResult();
 
         em.close();
 
-        request.setAttribute("tasks", tasks);
+        request.setAttribute("tasks",tasks);
+        request.setAttribute("tasks_count",tasks_count);
+        request.setAttribute("page",page);
 
         if(request.getSession().getAttribute("flush") != null){
             request.setAttribute("flush",request.getSession().getAttribute("flush"));
