77a8e4ff62e8a00f97576206634cd0b87358f512
yuya.nakase <red.arc1205@gmail.com>
2020-08-21 21:11:02 +0900

    Add validation

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
+import models.validators.TaskValidator;
 import utils.DBUtil;
 
 /**
@@ -45,6 +48,17 @@ public class CreateServlet extends HttpServlet {
             t.setCreated_at(currentTime);
             t.setUpdated_at(currentTime);
 
+            List<String> errors = TaskValidator.validate(t);
+            if(errors.size() > 0){
+                em.close();
+
+                request.setAttribute("_token",request.getSession().getId());
+                request.setAttribute("task",t);
+                request.setAttribute("errors",errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+                rd.forward(request, response);
+            }else{
             em.getTransaction().begin();
             em.persist(t);
             em.getTransaction().commit();
@@ -52,7 +66,7 @@ public class CreateServlet extends HttpServlet {
             em.close();
 
             response.sendRedirect(request.getContextPath() + "/index");
-
+            }
         }
     }
 
--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
+import models.validators.TaskValidator;
 import utils.DBUtil;
 
 /**
@@ -44,6 +47,18 @@ public class UpdateServlet extends HttpServlet {
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
             t.setUpdated_at(currentTime);
 
+            List<String> errors = TaskValidator.validate(t);
+            if(errors.size() > 0){
+                em.close();
+
+                request.setAttribute("_token",request.getSession().getId());
+                request.setAttribute("task",t);
+                request.setAttribute("errors",errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
+                rd.forward(request, response);
+            }else{
+
             em.getTransaction().begin();
             em.getTransaction().commit();
             request.getSession().setAttribute("flush","更新が完了しました。");
@@ -52,6 +67,7 @@ public class UpdateServlet extends HttpServlet {
             request.getSession().removeAttribute("task_id");
 
             response.sendRedirect(request.getContextPath() + "/index");
+            }
 
         }
     }
